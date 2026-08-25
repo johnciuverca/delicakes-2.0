75519afca908ff03431066d93a77d9fd506a160b
johnciuverca <johnciuverca@gmail.com>
2026-08-25 17:12:40 +0300

    fix: remove trailing whitespace

--- delicakes 2.0/app/main-ui/src/UserContext.tsx
+++ delicakes 2.0/app/main-ui/src/UserContext.tsx
@@ -5,7 +5,7 @@ export function UserProvider({ children}: { children: ReactNode}) {
     const [user, setUser] = useState<User | null>(null)
 
     return (
-        <UserContext.Provider value ={{ user, setUser }}>    
+        <UserContext.Provider value ={{ user, setUser }}>
         {children}
         </UserContext.Provider>
     )
